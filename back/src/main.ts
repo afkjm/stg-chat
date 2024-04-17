27f08b79524734b6884da8e94212d5561d8b9fac
jm <41496161+afkjm@users.noreply.github.com>
2024-04-18 06:24:27 +0900

    git merge --squash back20240416

---
+++ back/src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { Module } from '@nestjs/common';
+import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+
+@WebSocketGateway({path: '/back/'})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  handleConnection(client: any, ...args: any[]) {
+    console.log('Client connected:', client.id);
+  }
+
+  handleDisconnect(client: any) {
+    console.log('Client disconnected:', client.id);
+  }
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: any, payload: string): string {
+    return 'Message received: ' + payload;
+  }
+}
+
+@Module({
+  providers: [ChatGateway],
+})
+class AppModule {}
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
---
+++ front/src/contexts/SocketContext.tsx
@@ -0,0 +1,16 @@
+
+import { createContext } from 'preact'
+import { useContext } from 'preact/hooks'
+import { io } from 'socket.io-client'
+
+
+const socket = io('ws://localhost:8080', {
+  transports: ['websocket'],
+  path: '/back/',
+})
+const SocketContext = createContext();
+
+export const SocketContextProvider =
+  ({children}) => <SocketContext.Provider value={socket} children={children} />
+
+export const useSocketContext = () => useContext(SocketContext)
